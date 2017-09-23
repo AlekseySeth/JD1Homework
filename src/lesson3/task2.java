b3a64a435e9061fa78e97b77ee68ea8a2d7bb0b1
Aleksey <aleksey.v.sh@gmail.com>
2017-09-23 23:33:08 +0300

    Lesson 3 - Task 2

--- src/lesson3/task2.java
+++ src/lesson3/task2.java
@@ -11,7 +11,6 @@ import java.util.Scanner;
 public class task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
         System.out.println("Введите число:");
         int number = scanner.nextInt();
 
